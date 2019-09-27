8d32b4200ce3d45b6240e17f89be5f99393d593b
gaozhuangzhuang <1003978016@qq.com>
2019-09-27 18:21:18 +0800

    1

---
+++ medicine-base/src/main/java/com/medicine/medicinebase/MedicineBaseApplication.java
@@ -0,0 +1,13 @@
+package com.medicine.medicinebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicineBaseApplication.class, args);
+    }
+
+}
--- medicine-util/src/main/java/com/medicine/medicineutil/Comment.java
+++
@@ -1,4 +0,0 @@
-package com.medicine.medicineutil;
-
-public class Comment {
-}
